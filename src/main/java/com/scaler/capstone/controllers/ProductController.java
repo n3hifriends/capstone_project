bbb9638bcb3bfa4afdce878f5055ca7d982a98df
Nitin Kalokhe <nitinkalokhe@Nitins-MacBook-Pro.local>
2024-12-16 21:22:09 +0530

    response headed added

--- src/main/java/com/scaler/capstone/controllers/ProductController.java
+++ src/main/java/com/scaler/capstone/controllers/ProductController.java
@@ -6,7 +6,10 @@ import com.scaler.capstone.models.Product;
 import com.scaler.capstone.models.State;
 import com.scaler.capstone.services.IProductService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.util.LinkedMultiValueMap;
+import org.springframework.util.MultiValueMap;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.ArrayList;
@@ -40,14 +43,22 @@ public class ProductController {
 
     @GetMapping("{id}")
     public ResponseEntity<ProductDto> findProductById(@PathVariable Long id) {
+        MultiValueMap<String, String> params = new LinkedMultiValueMap<>();
         if(id <= 0){
-            return ResponseEntity.badRequest().build();
+            params.add("called by", "bhudwak");
+            return new ResponseEntity<>(null, params, HttpStatus.BAD_REQUEST);
+//            return ResponseEntity.badRequest().headers(params).build();
         }
         Product product = productService.getProductById(id);
         if( product == null){
-            return ResponseEntity.notFound().build();
+            return new ResponseEntity<>(null, params, HttpStatus.NOT_FOUND);
+
+//            return ResponseEntity.notFound().build();
         }
-        return ResponseEntity.ok(toProductDto(product));
+        params.add("called by", "Intelligent");
+        return new ResponseEntity<>(null, params, HttpStatus.OK);
+
+//        return ResponseEntity.ok(toProductDto(product));
     }
 
     public static ProductDto toProductDto(Product product) {
