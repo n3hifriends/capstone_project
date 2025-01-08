ab1ac8cf5bf4692f59ed984d9f8c91ec3f40efc6
Nitin Kalokhe <nitinkalokhe@Nitins-MacBook-Pro.local>
2025-01-08 21:27:28 +0530

    fetchTypes testing & behaviour

--- src/main/java/com/scaler/capstone/models/Category.java
+++ src/main/java/com/scaler/capstone/models/Category.java
@@ -2,12 +2,12 @@ package com.scaler.capstone.models;
 
 import java.util.List;
 
-import jakarta.persistence.Entity;
-import jakarta.persistence.FetchType;
-import jakarta.persistence.OneToMany;
-import jakarta.persistence.OneToOne;
+import jakarta.persistence.*;
 import lombok.Getter;
 import lombok.Setter;
+import org.hibernate.annotations.BatchSize;
+import org.hibernate.annotations.Fetch;
+import org.hibernate.annotations.FetchMode;
 
 @Getter
 @Setter
@@ -16,6 +16,19 @@ public class Category extends BaseModel{
     private String name;
     private String description;
 
-    @OneToMany(fetch = FetchType.EAGER, mappedBy = "category") // to avoid circular dependency with Product class
+    @OneToMany(fetch = FetchType.LAZY, orphanRemoval = true)// to avoid circular dependency with Product class
+    @Fetch(FetchMode.SELECT)
     private List<Product> products;
+
+    // try with
+    // LAZY, EAGER
+    // SELECT, JOIN, SUB SELECT
+    // getProduct or doNot call getProduct
+
+
+    // Execute query in batches
+    // @Fetch(FetchMode.SELECT)
+    // @BatchSize(size = 2)
+    // private List<Product> products;
+
 }
--- src/test/java/com/scaler/capstone/repos/CategoryRepoTest.java
+++ src/test/java/com/scaler/capstone/repos/CategoryRepoTest.java
@@ -7,6 +7,8 @@ import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 
+import java.util.List;
+
 import static org.junit.jupiter.api.Assertions.*;
 
 @SpringBootTest
@@ -21,9 +23,20 @@ class CategoryRepoTest {
         Category category = categoryRepo.findById(1L).get();
 //        category.getProducts().forEach(System.out::println); // fetch will happen LAZY & will not load from DB
         for(Product product : category.getProducts()){ // fetch will happen and will load from DB
-            System.out.println(product);
+            System.out.println(product.getName());
         }
 
         //Note: When EAGER -> it will always fetch product
+    }
+
+    @Test
+    @Transactional
+    void testSomething(){
+        List<Category> catList = categoryRepo.findAll();
+        for(Category category : catList){
+            for(Product product : category.getProducts()){
+                System.out.println(product.getName());
+            }
+        }
     }
 }
